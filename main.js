d87569965bdf656bf2cbfbaf2a5cc5fe28bb53cb
GonDragon <gondragon13@gmail.com>
2026-05-15 19:55:57 -0300

    punto5

--- main.js
+++ main.js
@@ -1,5 +1,14 @@
-function traerCartas(cantidad){
-    return fetch(`https://deckofcardsapi.com/api/deck/new/draw/?count=${cantidad}`);
+const paginador = {
+    index: 0,
+    paginas: [[]],
+
+    agregarAPagina: function(carta) {
+        if(this.paginas.length <= this.index){
+            this.paginas.push([]);
+        }
+
+        this.paginas[this.index].push(carta);
+    }
 }
 
 function limpiarCartas(){
@@ -8,30 +17,58 @@ function limpiarCartas(){
 }
 
 function agregarCarta(ObjetoCartaRaw){
-    const contenedorCartas = document.getElementById("cartas");
-
-    console.log(ObjetoCartaRaw);
     const carta = Carta.createFromJsonString(JSON.stringify(ObjetoCartaRaw));
-    const cartaHtml = carta.createHtmlElement();
-    contenedorCartas.appendChild(cartaHtml);
+    paginador.agregarAPagina(carta);
+}
+
+function traerCartas(cantidad){
+    let cartas = fetch(`https://deckofcardsapi.com/api/deck/new/draw/?count=${cantidad}`);
+
+    return cartas.then(respuesta => {
+        return respuesta.json();
+    }).then( cartas => {
+
+        for (let index = 0; index < cartas.cards.length; index++) {
+            agregarCarta(cartas.cards[index]);
+        }
+    });
+}
+
+function actualizarCartasHTML(){
+    limpiarCartas();
+    // console.log(paginador);
+    const contenedorCartas = document.getElementById("cartas");
+    let paginaActual = paginador.paginas[paginador.index];
+    for (let index = 0; index < paginaActual.length -1; index++) {
+        console.log("loop");
+        const carta = paginaActual[index];      
+        const cartaHtml = carta.createHtmlElement();
+        contenedorCartas.appendChild(cartaHtml);
+    }
+}
+
+function paginaSiguiente(){
+    paginador.index++;
+
+    if(paginador.index >= paginador.paginas.length) {
+        traerCartas(6).then( () => {actualizarCartasHTML();});
+    } else {
+        actualizarCartasHTML();
+    }
+    console.log(`indice ${paginador.index}`);
+}
+
+function paginaAnterior(){
+    if(paginador.index <= 0) return;
+    paginador.index--;
+    actualizarCartasHTML();
+    console.log(`indice ${paginador.index}`);
 }
 
 
 /* Codigo Suelto Desde Aca */
 console.log("Inicio del codigo suelto");
 
-let cartas = traerCartas(6);
-
-cartas.then(respuesta => {
-    return respuesta.json();
-}).then( cartas => {
-    console.log(cartas);
-    limpiarCartas();
-
-    for (let index = 0; index < cartas.cards.length; index++) {
-        agregarCarta(cartas.cards[index]);
-        
-    }
-});
+traerCartas(6).then( () => {actualizarCartasHTML();});
 
 console.log("Fin del codigo suelto");
